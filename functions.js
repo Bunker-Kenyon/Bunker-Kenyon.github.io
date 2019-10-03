0c3611cd720a26af74ef752bf23e2649eb28c7c6
Kenyon Bunker <kjbunker817@gmail.com>
2019-10-02 18:25:55 -0700

    Finished JSBasics
    Finished JSBasics
    Created and filled functions.js

--- functions.js
+++ functions.js
@@ -1,338 +1,151 @@
-function setDefaults()
-{
-    document.getElementById("musicprice").value = 0;
-    document.getElementById("lessonprice").value = 0;
-    document.getElementById("subtotal").value = 0;
-    document.getElementById("shippingcost").value = 0;
-    document.getElementById("taxes").value = 0;
-    document.getElementById("total").value = 0;
+function setDefaults() {
 
 }
 
-function addSheetMusic()
-{
-    var musicTotal = 0;
+function calcVar() {
 
-    var music = document.querySelectorAll('#sheetmusic input');
-    for (var i = 0; i < music.length; i++)
-    {
-        if (music[i].checked == true)
-        {
-            musicTotal = musicTotal + Number(music[i].value);
-        }
+    var x = 0;
+    var y = 0;
+    var varTotal = 0;
+
+    x = Number(document.getElementById("varInputX").value);
+    y = Number(document.getElementById("varInputY").value);
+
+    varTotal = x + y;
+
+    document.getElementById("varOutput").innerHTML = varTotal;
+}
+
+function calcFuncPram(x, y) {
+    var funcTotal = 0;
+    funcTotal = Number(x * y);
+    document.getElementById("funcOutput").innerHTML = funcTotal;
+}
+
+function forLoop(movies) {
+
+    movies.push(document.getElementById("movie").value);
+
+    document.getElementById("movie").value = "";
+
+    var outputText = "";
+    var i;
+    for (i = 0; i < movies.length; i++) {
+        outputText += movies[i] + "<br>";
     }
+    console.log(movies);
 
-    document.getElementById("musicprice").innerHTML = "$" + musicTotal.toFixed(2);
-    document.getElementById("musicprice").value = musicTotal;
-
-    
-    
-
-    calcTotal();
+    document.getElementById("forLoopOutput").innerHTML = outputText;
 }
 
-function addLessons()
-{
-    var basecost = 105;
-    var lessoncost = 0;
-    var months = 0;
-    var months = document.getElementById("lessonduration").value;
+function calcWhileLoop() {
 
-    if (months > 0 && document.getElementById("instrament").value == "default")
-    {
-        document.getElementById("instramenterror").style.visibility = 'visible';
+    var num = 0
+    var max = 12;
+    
+
+    num = Number(document.getElementById("whileLoopInput").value);
+    var output = 0;
+
+    var text = "";
+    var i = 1;
+    while (i < max + 1) {
+        output = num * i;
+        text += "<br>" + output;
+        i++;
+       
+    }
+    document.getElementById("whileLoopOutput").innerHTML = text;
+}
+
+function calcDoWhileLoop() {
+
+    var num = 0
+    var max = 12;
+
+    num = Number(document.getElementById("doWhileLoopInput").value);
+    var output = 0;
+
+    var text = "";
+    var i = 1;
+    do {
+        output = num * i;
+        text += "<br>" + output;
+        i++;
+    }
         
-    }
-    else
-    {
-        document.getElementById("instramenterror").style.visibility = 'hidden';
-    }
-
-    if (document.getElementById("instrament").value != "default" && months <= 0)
-    {
-        document.getElementById("durationerror").style.visibility = 'visible';
-    }
-    else
-    {
-        document.getElementById("durationerror").style.visibility = 'hidden';
-    }
-    
-    lessoncost = months * basecost;
-    document.getElementById("lessonprice").innerHTML = "$" + lessoncost.toFixed(2);
-    document.getElementById("lessonprice").value = lessoncost;
-
-    calcTotal();
-
-}
-
-function calcSubtotal()
-{
-
-
-    var subtotal = 0;
-    var musicTotal = 0;
-    var lessonTotal = 0;
-
-    //alert(document.getElementById("lessonprice").value);
-    musicTotal = document.getElementById("musicprice").value;
-    lessonTotal = document.getElementById("lessonprice").value;
-
-    subtotal = musicTotal + lessonTotal;
-    document.getElementById("subtotal").value = subtotal;
-    document.getElementById("subtotal").innerHTML = "$" + subtotal.toFixed(2);
-
-    calcTaxes(subtotal);
-
-
-    
-}
-
-function calcTaxes(subTotal)
-{
-    var taxPercentage = .07;
-    var tax = 0;
-
-    tax = taxPercentage * subTotal;
-
-    document.getElementById("taxes").innerHTML = "$" + tax.toFixed(2);
-    document.getElementById("taxes").value = tax;
-    
-}
-
-function calcShipping()
-{
-    var shippingRate = 1.05;
-    var musicPrice = 0;
-    musicPrice = document.getElementById("musicprice").value;
-    if (musicPrice > 0)
-    {
-        document.getElementById("shippingcost").value = shippingRate;
-        document.getElementById("shippingcost").innerHTML = "$" + shippingRate;
-    }
-    else {
-        document.getElementById("shippingcost").value = 0;
-    }
-}
-
-function calcTotal()
-{
-    calcSubtotal();
-    calcShipping();
-    
-    var total = 0;
-    var subtotal = 0;
-    var taxes = 0;
-    var shippingCost = 0;
-
-    subtotal = document.getElementById("subtotal").value;
-    shippingCost = document.getElementById("shippingcost").value;
-    taxes = document.getElementById("taxes").value;
-
-    total = subtotal + shippingCost + taxes;
-
-    
-
-    document.getElementById("total").value = total;
-    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
-
-}
-
-function checkFirstName()
-{
-    var input = document.getElementById("fname").value;
-    if (input.search(/^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$/) >= 0)
-    {
-        document.getElementById("fnerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("fnerror").style.visibility = 'visible';
-    }
-}
-
-function checkLastName()
-{
-    var input = document.getElementById("lname").value;
-    if (input.search(/^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$/) >= 0)
-    {
-        document.getElementById("lnerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("lnerror").style.visibility = 'visible';
-    }
-}
-
-function checkEmail()
-{
-    var input = document.getElementById("email").value;
-    if (input.search(/^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/) >= 0)
-    {
-        document.getElementById("emailerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("emailerror").style.visibility = 'visible';
-    }
-}
-
-function checkPhone()
-{
-    var input = document.getElementById("phone").value;
-    if (input.search(/(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/) >= 0)
-    {
-        document.getElementById("phoneerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("phoneerror").style.visibility = 'visible';
-    }
-}
-
-function checkAddress()
-{
-    var input = document.getElementById("address").value;
-    if (input.search(/(\d{1,5}\s(N|S|E|W)(\.?\s\w+))|([1-9]{1,5}\s\w{3,})/) >= 0) {
-        document.getElementById("addresserror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("addresserror").style.visibility = 'visible';
-    }
-}
-
-function checkApt()
-{
-    var input = document.getElementById("apt").value;
-    if (input.search(/\d{1,5}/) >= 0) {
-        document.getElementById("apterror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("apterror").style.visibility = 'visible';
-    }
-}
-
-function checkCity() {
-    var input = document.getElementById("city").value;
-    if (input.search(/[A-Z][a-z]{2,}/) >= 0) {
-        document.getElementById("cityerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("cityerror").style.visibility = 'visible';
-    }
-}
-
-function checkState() {
-    var input = (document.getElementById("state").value).toUpperCase();
-    document.getElementById("state").value = input;
-
-    var states = new Array('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
-        + 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-        + 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
-        + 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
-        + 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'AS', 'GU', 'MH', 'FM', 'MP', 'PW', 'PR', 'VI');
-
-    for (var s = 0; s < states.length; s++)
-    {
-        if (input == states[s])
-        {
-            document.getElementById("stateerror").style.visibility = 'hidden';
-            { break; }
-            alert("success")
-        }
-        else {
-            document.getElementById("stateerror").style.visibility = 'visible';
-        }
-    }
-}
-
-function checkZip()
-{
-    var input = document.getElementById("zip").value;
-    if (input.search(/[0-9]{5}/) >= 0) {
-        document.getElementById("ziperror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("ziperror").style.visibility = 'visible';
-    }
-}
-
-function checkCardNetwork()
-{
-    var input = document.getElementById("cardnetwork").value;
-
-    if (input != "default") {
-        document.getElementById("cnerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("cnerror").style.visibility = 'visible';
-    }
-}
-
-function checkCardNum()
-{
-    var input = document.getElementById("cardnum").value;
-    if (input.search(/([0-9]{16})/) >= 0)
-    {
-        document.getElementById("cnumerror").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("cnumerror").style.visibility = 'visible';
-    }
-}
-
-function checkExpDate()
-{
-    var input = document.getElementById("expdate").value;
-    var div = input.split("/")
-
-    var month = Number(div[0], 10);
-    var year = Number(div[1], 10);
-
-    if (input.search(/\d{2}\/\d{2}/) >= 0)
-    {
-        if (year < 18 || year > 30 || month <= 0 || month > 12)
-        {
-            document.getElementById("expdateerror").style.visibility = 'visible';
-        }
-        else
-        {
-            document.getElementById("expdateerror").style.visibility = 'hidden';
-        }
+    while (i < max + 1) {
         
-    } 
-    else
-    {
-        document.getElementById("expdateerror").style.visibility = 'visible';
+
     }
+    document.getElementById("doWhileLoopOutput").innerHTML = text;
 }
 
-function checkSecCode()
-{
-    var input = document.getElementById("seccode").value;
-    if (input.search(/\d{3}/) >= 0) {
-        document.getElementById("scerror").style.visibility = 'hidden';
+function calcIfElse() {
+
+
+    var age = 0;
+    var baby = 2;
+    var child = 12;
+    var senior = 55;
+    
+    age = Number(document.getElementById("ifElseInput").value);
+
+    if (age <= baby) {
+        document.getElementById("ifElseOutput").innerHTML = "Babys get in for free.";
+    }
+    else if (age <= child) {
+        document.getElementById("ifElseOutput").innerHTML = "Children get a 10% discount.";
+    }
+    else if (age < senior) {
+        document.getElementById("ifElseOutput").innerHTML = "Teens and Adults get in at regular price.";
+    }
+    else if (age >= 55) {
+        document.getElementById("ifElseOutput").innerHTML = "Seniors get a 15% discount.";
     }
     else {
-        document.getElementById("scerror").style.visibility = 'visible';
+        document.getElementById("ifElseOutput").innerHTML = "Error: try again.";
     }
 }
 
-function submitForms()
-{
-    document.getElementById("sheetmusic").submit();
-    document.getElementById("lessons").submit();
-    document.getElementById("custinfo").submit();
-    document.getElementById("shippinginfo").submit();
-    document.getElementById("payment").submit();
-    document.getElementById("totals").submit();
-    document.getElementById("subnotice").style.visibility = 'visible'; 
-}
+function pickMovie() {
+    var text = "";
+    var movie = document.getElementById("switchInput").value;
 
-function resetForms()
-{
-    document.getElementById("sheetmusic").reset();
-    document.getElementById("lessons").reset();
-    document.getElementById("custinfo").reset();
-    document.getElementById("shippinginfo").reset();
-    document.getElementById("payment").reset();
-    document.getElementById("totals").reset();
-    document.getElementById("resnotice").style.visibility = 'visible';
-    setDefaults();
-}
+    switch (movie) {
+        case "1":
+            text = "Now, this is podracing!";
+            break;
+        case "2":
+            text = "I don't like sand... Ok that was the worst scene in Sta Wars...";
+            break;
+        case "3":
+            text = "I was your brother, Anakin!!!";
+            break;
+        case "4":
+            text = "Trust the force, Luke. Let go, Luke.";
+            break
+        case "5":
+            text = "No, I am your father.";
+            break
+        case "6":
+            text = "You all ready have Luke, you were right, you were right about me, tell your sister, you were right.";
+            break
+        case "7":
+            text = "Chewie, We're Home.";
+            break
+        case "8":
+            text = "I only know one truth: It's time for the Jedi to end.";
+            break
+        case "Solo":
+            text = "I just did the Kessel Run in twelve parsecs.";
+            break
+        case "Rouge One":
+            text = "Rebellions are built on hope.";
+            break
+        default:
+            text = "I can't wait for Rise of Skywalker!";
+    }
 
+    document.getElementById("switchOutput").innerHTML = text;
+}
