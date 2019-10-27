0239963a667c40e7b33e6c181cc63950ed791342
Kenyon Bunker <kjbunker817@gmail.com>
2019-10-26 17:25:42 -0700

    working on jsevents

--- functions.js
+++ functions.js
@@ -252,3 +252,22 @@ function getWeather(key) {
 
 
 }
+
+function makeTable2() {
+    document.getElementById("instructions").style.visibility = "visible";
+    var table = document.getElementById("base");
+    for (var i = 0; i < 10; i++) {
+        var row = document.createElement("tr");
+        for (var j = 0; j < 10; j++) {
+            var col = document.createElement("td");
+            var img = document.createElement('img');
+            img.src = "stargate.png";
+            img.align = "middle";
+            img.style = "width: 40px; height: 40px";
+            col.appendChild(img);
+            row.appendChild(col);
+        }
+        table.appendChild(row);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+}
