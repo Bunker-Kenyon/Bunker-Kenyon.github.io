3c337688b5e4ec5c3b2ff61d4be1ef6ad2be5798
Kenyon Bunker <kjbunker817@gmail.com>
2019-11-14 19:46:37 -0700

    Update Board.js

--- project/js/Board.js
+++ project/js/Board.js
@@ -3,6 +3,6 @@ import { Weather } from './Weather.js';
 (function () {
     const weather = new Weather();
 
-    weather.getWeather(85140, getKey());
+    weather.getWeather(85140, weather.getKey());
 
 }) ();
