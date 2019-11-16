fde8eb9bf0e7bbcf63f5bbc3f24b6fcb19016790
Kenyon Bunker <kjbunker817@gmail.com>
2019-11-16 08:00:03 -0700

    Update Weather.js

--- project/js/Weather.js
+++ project/js/Weather.js
@@ -1,8 +1,12 @@
 export class Weather {
     weather(zipCode, key) {
         this.key = key;
-        this.zipCode = zipCode
+        this.zipCode = zipCode;
+        var city = '';
+        var tempurature = '';
+        var condition = '';
     }
+    
     
     getKey() {
         let apiID = "3c1f0026935cf8c5b2695f49e552d817";
@@ -31,5 +35,28 @@ export class Weather {
         xmlhttp.send();
 
         return localWeather;
+    }
+
+    displayWeather() {
+        localWeather = getWeather(this.zipCode, this.key);
+        let weatherDetails = JSON.parse(localWeather);
+
+        city = weatherDetails.name;
+        tempurature = weatherDetails.main.temp;
+        condition = weatherDetails.weather.main;
+
+        console.log(city, + ", " + tempurature, ", " + condition)
+
+        /*
+            Weather conditon types:
+                Thunderstorm
+                Drizzle
+                Rain
+                Snow
+                Clear
+                Clouds
+                Other (haze, fog, etc)
+
+        */
     }
 }
