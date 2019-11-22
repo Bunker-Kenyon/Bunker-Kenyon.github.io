98f40341860eb9ba28202b8b4157e1b508adbfa6
Kenyon Bunker <kjbunker817@gmail.com>
2019-11-22 05:52:17 -0700

    update

--- project/js/Board.js
+++ project/js/Board.js
@@ -5,9 +5,25 @@ window.populateWeather = function(zipCode) {
     const key = weather.getKey();
 
     weather.calcWeather(zipCode, key, (thisWeather) => {
-        document.getElementById("temp").innerHTML = "<span>" + weather.tempurature + "&deg</span>";
+        document.getElementById("temp").innerHTML = "<span>" + weather.getTempurature() + "&deg</span>";
+        document.getElementById("condition").innerHTML = weather.getCondition();
+        document.getElementById("plc").innerHTML = weather.getCity();
+        document.getElementById("dt").innerHTML = formatDate(new Date());
     });
 
-    var x = 0;
+    function formatDate(date) {
+        var monthNames = [
+          "Jan", "Feb", "March",
+          "April", "May", "June", "July",
+          "Aug", "Sep", "Oct",
+          "Nov", "Dec"
+        ];
+      
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+      
+        return day + ' ' + monthNames[monthIndex] + ' ' + year;
+      }
     
 }
